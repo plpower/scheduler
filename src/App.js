899123c5d2fe0af270d17f3fa94751744105e8fb
Patrice Power <patricepower2021@u.northwestern.edu>
2020-04-07 21:54:06 -0400

    Slice 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
@@ -28,15 +28,30 @@ const schedule = {
   ]
 };
 
-const App = () => (
-  <Container>
-    <Banner title={schedule.title} />
-    <CourseList courses={schedule.courses} />
-  </Container>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+ 
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
+  return (
+    <Container>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </Container>
+  );
+};
 
 const Banner = ({ title }) => (
-  <Title>{title}</Title>
+  <Title>{title || '[loading...]'}</Title>
 );
 
 const CourseList = ({ courses }) => (
